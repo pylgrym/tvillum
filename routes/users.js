91a71b1c59ddde5ca63a442cb55b5bcebb78a6b5
Jakob Gaardsted <jgaardsted@gmail.com>
2015-01-01 22:59:38 +0100

    user login create layout

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  console.log('user route');
+  res.render('users', 
+    { 
+      username: 'john@funnyfield.com',
+      password: '***not',
+      loggedIn: false
+      //item: subj_list[curPage_cl], 
+      //curPage_jd: curPage_cl,
+      //comments: comments
+    }
+  );  
+
 });
 
 module.exports = router;
