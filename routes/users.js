bcb732d304c14bfc1685f03857a4c4c5627b5597
Jakob Gaardsted <jgaardsted@gmail.com>
2015-01-02 10:43:51 +0100

    login states

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+  var badLogin     = (req.query.loggedIn == 2);
+  var unknownLogin = (req.query.loggedIn == 3);
   console.log('user route');
   // params, we'd get from the path.
   //console.log('user', req.params.username);
-  console.log('user', req.query.username);
+  console.log('user', req.query.username);  
   res.render('users', 
     { 
       username: req.query.username,
       password: req.query.password,
-      loggedIn: req.query.loggedIn
+      loggedIn: (req.query.loggedIn==1),
+      badLogin: badLogin,
+      unknownLogin: unknownLogin
     }
   );  
 
